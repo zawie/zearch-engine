1d94252b13d660e376f7150d76e8796056aa69be
zawie <zawie@rice.edu>
2022-12-20 17:09:43 -0800

    parseMetaDataTest

--- tst/zearch/spider/scraper/ScraperTest.java
+++ tst/zearch/spider/scraper/ScraperTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,10 +17,17 @@ class ScraperTest {
         Document doc = Scraper.getDocumentFromFilepath("tst/html/simple.html");
         Queue<String> links = new LinkedList<>();
         Scraper.parseLinks("https://www.sample.com", doc, links);
-        System.out.println(links);
         assertTrue(links.contains("https://www.sample.com/something1"));
         assertTrue(links.contains("https://www.sample.com/something2"));
         assertTrue(links.contains("https://www.sample.com/something3"));
         assertTrue(links.contains("https://www.zawie.io"));
+    }
+    @Test
+    void parseMetaDataTest() throws IOException {
+        Document doc = Scraper.getDocumentFromFilepath("tst/html/simple.html");
+        Map<String, String> metaData = Scraper.parseMetaData(doc);
+        assertEquals(metaData.get("title"), "Test Title");
+        assertEquals(metaData.get("description"), "test description");
+        assertEquals(metaData.get("keywords"), "test, sample, word");
     }
 }
