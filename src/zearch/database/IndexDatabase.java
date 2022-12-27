85d1029dead31006d67ad576f5e3bebfeab24c6f
zawie <zawie@rice.edu>
2022-12-26 23:49:23 -0800

    unique id per row

--- src/zearch/database/IndexDatabase.java
+++ src/zearch/database/IndexDatabase.java
@@ -24,8 +24,9 @@ public class IndexDatabase {
         Statement statement = connection.createStatement();
 
         StringBuilder columns = new StringBuilder();
-        columns.append("`url` VARCHAR(2048) PRIMARY KEY");
-        columns.append(", ").append("Timestamp DATETIME NOT NULL DEFAULT(CURRENT_TIMESTAMP)");
+        columns.append("`id` BIGINT AUTO_INCREMENT PRIMARY KEY");
+        columns.append(", ").append("`url` VARCHAR(2048)");
+        columns.append(", ").append("`timestamp` DATETIME NOT NULL DEFAULT(CURRENT_TIMESTAMP)");
         columns.append(", ").append("`title` VARCHAR(128)");
         columns.append(", ").append("`description` VARCHAR(1024)");
         columns.append(", ").append("`keywords` VARCHAR(256)");
@@ -57,20 +58,27 @@ public class IndexDatabase {
         List<String> columns = new LinkedList<>();
         List<String> values = new LinkedList<>();
 
+        columns.add("url");
         values.add("'"+url+"'");
+        columns.add("timestamp");
         values.add("CURRENT_TIMESTAMP");
+        columns.add("title");
         values.add("'" + boundString(metaData.getOrDefault("title",""), 128).replaceAll("'", "''")          + "'");
+        columns.add("description");
         values.add("'" + boundString(metaData.getOrDefault("description",""), 1024).replaceAll("'", "''")        + "'");
+        columns.add("keywords");
         values.add("'" + boundString(metaData.getOrDefault("keywords",""), 256).replaceAll("'", "''")           + "'");
+        columns.add("author");
         values.add("'" + boundString(metaData.getOrDefault("author",""), 64).replaceAll("'", "''")             + "'");
 
         assert(hashes.length == MinHasher.COUNT);
         for (int h = 0; h < MinHasher.COUNT; h++) {
             String hex = Integer.toHexString(hashes[h]);
             while (hex.length() < 2) hex = "0" + hex;
+            columns.add("`hash"+h+"`");
             values.add("X'" + hex + "'");
         }
-        statement.execute("DELETE FROM index_table WHERE `url` = '"+url+"';");
-        statement.execute("INSERT INTO index_table VALUES (" +String.join(", ", values)+ ")");
+//        statement.execute("DELETE FROM index_table WHERE `url` = '"+url+"';"); //TODO: Remove duplicate entries periodically
+        statement.execute("INSERT INTO index_table ("+String.join(", ", columns)+") VALUES (" +String.join(", ", values)+ ")");
     }
 }
