30a4dfbce1e7de6d82a645dabd61b640a22b5a78
zawie <zawie@rice.edu>
2022-12-29 14:01:17 -0800

    add quotes to search script

--- search.sh
+++ search.sh
@@ -1,1 +1,1 @@
-java -cp ./out:./lib/jsoup-1.15.3.jar:./lib/h2-2.1.214.jar zearch.Controller $(cat ./database_filepath) 0 false $@
+java -cp ./out:./lib/jsoup-1.15.3.jar:./lib/h2-2.1.214.jar zearch.Controller $(cat ./database_filepath) 0 false "$@"
