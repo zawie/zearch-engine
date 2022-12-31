fc83db5bfe9877a0924c7a90699060573ab2a54c
zawie <zawie@rice.edu>
2022-12-31 13:18:33 -0800

    change https protocol

--- src/zearch/server/Server.java
+++ src/zearch/server/Server.java
@@ -23,7 +23,7 @@ public class Server {
     public Server(IServerToModel model) throws IOException, NoSuchAlgorithmException, KeyStoreException, CertificateException, UnrecoverableKeyException, KeyManagementException {
         this.model = model;
         this.server = HttpsServer.create(new InetSocketAddress(PORT), 0);
-        this.sslContext = SSLContext.getInstance("TLS");
+        this.sslContext = SSLContext.getInstance("SSL");
 
         // Initialise the keystore
         char[] password = "simulator".toCharArray();
