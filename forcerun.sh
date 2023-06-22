eede4c8ef058a74c21feca17afe7d342ac8c9143
zawie <zawie@rice.edu>
2023-06-22 11:40:57 -0500

    run main in background

--- forcerun.sh
+++ forcerun.sh
@@ -8,4 +8,4 @@ make
 kill -9 `ps -C java -o pid=`
 
 # Run the program
-./scripts/monolith/main.sh https://en.wikipedia.org/wiki/List_of_most_visited_websites > output.log
+./scripts/monolith/main.sh https://en.wikipedia.org/wiki/List_of_most_visited_websites > output.log &
