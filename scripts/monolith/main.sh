2f3ac3793b11bc8c32deb2883693179ac32400a6
zawie <zawie@rice.edu>
2023-06-22 12:26:29 -0500

    increase num crawler threads

--- scripts/monolith/main.sh
+++ scripts/monolith/main.sh
@@ -1,1 +1,1 @@
-java -Xmx3g -cp ./out:./lib/jsoup-1.15.3.jar:./lib/h2-2.1.214.jar zearch.controller.Monolith $(cat ./database_filepath) 2 true $@
+java -Xmx3g -cp ./out:./lib/jsoup-1.15.3.jar:./lib/h2-2.1.214.jar zearch.controller.Monolith $(cat ./database_filepath) 8 true $@
