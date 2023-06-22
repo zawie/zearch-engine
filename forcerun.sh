f4a228e229e9fbd9a44935ba4f9af3f5a3c48757
zawie <zawie@rice.edu>
2023-06-22 11:31:35 -0500

    fix killall

--- forcerun.sh
+++ forcerun.sh
@@ -5,7 +5,7 @@ git pull
 make
 
 # Stop all currently running java tasks
-killall -9 java
+kill -9 `ps -C java -o pid=`
 
 # Run the program
 ./scripts/monolith/main.sh https://en.wikipedia.org/wiki/List_of_most_visited_websites > output.log
