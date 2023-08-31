94d9923337ed5c2e3f572e722d70005cc360f7d3
Adam Zawierucha <zawiedev@gmail.com>
2023-08-30 18:40:07 -0700

    Update forcerun.sh

--- forcerun.sh
+++ forcerun.sh
@@ -7,6 +7,6 @@ make
 # Stop all currently running java tasks
 kill -9 `ps -C java -o pid=`
 
-SITES=$(shuf -n 50 data/top500sites.txt | sed ':a;N;$!ba;s/\n/ /g')
+SITES=$(shuf -n 50 data/top500sites.txt | sed 's/\n/ /g')
 # Run the program
 nohup ./scripts/monolith/main.sh $SITES > engine.log 2> engine.err < /dev/null &
